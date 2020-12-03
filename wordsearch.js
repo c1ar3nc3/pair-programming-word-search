f73b9020c7a35e19f1158727c532efd3b156987c
Clarence Williams <xwilliams.clarencex@gmail.com>
2020-12-03 07:00:11 +0000

    peer programmed w/ Jared

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//Peer Programmed with Jared Mosher
 
-module.exports = wordSearch
+
+const wordSearch = (letters, word) => { 
+    const horizontalJoin = letters.map(l => l.join(''))
+    const verticalJoin = transpose(letters).map(l => l.join(''));
+    
+
+    for (let row of horizontalJoin) {
+        if (row.includes(word)) {
+            return true;
+        }
+    };
+
+    for (let col of verticalJoin) {
+        if (col.includes(word)) {
+            return true;
+        }
+    };
+    return false;
+};
+
+const transpose = function(letters) {
+let arr = [];
+    for (let i = 0; i < letters[0].length; i++) {
+      arr.push([]);
+    }
+    for (let row = 0; row < letters.length; row++) {
+      for (let col = 0; col < letters[row].length; col++) {
+        arr[col][row] = letters[row][col];
+      }
+    }
+    return arr;
+  };
+
+module.exports = wordSearch;
